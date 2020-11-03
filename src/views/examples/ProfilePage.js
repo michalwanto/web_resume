26dfd8a926e131e57015875995a35a7e9498971c
michalwanto <59409764+michalwanto@users.noreply.github.com>
2020-11-03 11:41:01 +1100

    fixed the expereinces page

--- src/views/examples/ProfilePage.js
+++ src/views/examples/ProfilePage.js
@@ -125,7 +125,7 @@ function ProfilePage() {
                   <Col className="ml-auto mr-auto" md="10">
                   <h3 className="ml-auto mr-auto">Education</h3>
                   <hr style={{width:"70vw"}}></hr>
-                  <Row className="collections">
+                  <Row style={{marginBottom:"50px"}} className="collections">
                       <Col md="5">
                         <img
                           alt="..."
@@ -134,7 +134,7 @@ function ProfilePage() {
                           style={{aligntItems:"centre", width:"200px",maxHeight:"400px", marginLeft: "70px"}}
                         ></img>
                       </Col>
-                      <Col md="6">
+                      <Col  md="6">
                         <h4 style={{marginBottom:"2px"}}>SEKOLAH GLOBAL INDO-ASIA </h4>
                         <h5 style={{marginBottom:"2px",color:"grey"}}>High School</h5>
                         <p>2010-2016</p>
@@ -172,34 +172,57 @@ function ProfilePage() {
                         <img
                           alt="..."
                           className="img-raised"
-                          src="https://primarylibrary.sgiaedu.org/template/default/assets/images/Logo.png"
+                          src={require("assets/img/IFASHIONGROUP.png")}
                           style={{aligntItems:"centre", width:"200px",maxHeight:"400px", marginLeft: "70px"}}
                         ></img>
                       </Col>
-                      <Col md="6">
-                        <h4 style={{marginBottom:"2px"}}>SEKOLAH GLOBAL INDO-ASIA </h4>
-                        <h5 style={{marginBottom:"2px",color:"grey"}}>High School</h5>
-                        <p>2010-2016</p>
-                        <span>O-Level and A-Level courses. Coursework included Mathematics, Business Studies, Economics, Global Perspective, Bahasa Indonesia, English. Average Transcript of 85.</span>
+                      <Col style={{marginBottom:"50px"}} md="6">
+                        <h4 style={{marginBottom:"2px"}}>IFASHION GROUP </h4>
+                        <h5 style={{marginBottom:"2px",color:"grey"}}>Finance Intern</h5>
+                        <p style={{marginBottom:"2px",fontSize:"13px"}}>May 2018 - Aug 2018</p>
+                        <p style={{fontSize:"12px"}}>Singapore</p>
+                        <span>Worked closely with CFO in managing financial data, ad-hoc entries, bank reconciliation, account receivables & payables reconciliation, and invoicing(through Xero accounting platform), this includes recording and data entry in Xero, analyzing exported data in excel using macros, pivot tables, V-lookup, conditional formatting, etc. Involved in preparing the company’s FP&A, credit risk management & modeling, margin analysis , fixing HR handbook in term of policies, regulations, and procedures for the company’s subsidiaries that operate across Southeast Asian countries from Indonesia, Vietnam, Thailand, Malaysia, Laos, to Singapore and present it to the internal stakeholders suggesting ways in improving its subsidiaries daily operations, protocols, documentation, financial control & management and HR coordination.</span>
+                        <br></br>
+                        <br></br>
+                        <span >Managed $1M+ worth of Inventories Data of its four subsidiary companies (Dressabelle, Invade, Red Republics, and Megafash). Gathering missing data, cleansing of data, analyzing data, designing and formulating charts, diagrams, dashboards, and provide key metrics and recommendations to the CFO.</span>
                       </Col>
                     </Row>
+                    <hr></hr>
                     <Row className="collections">
                     <Col md="5">
                     
                         <img
                           alt="..."
                           className="img-raised"
-                          src={require("assets/img/SPJAIN.png")}
+                          src={require("assets/img/GOFLEETINT.png")}
                           style={{aligntItems:"centre", width:"200px",maxHeight:"400px", marginLeft: "70px"}}
                         ></img>
                       </Col>
                       <Col md="6">
-                        <h4 style={{marginBottom:"2px"}}>SP JAIN SCHOOL OF GLOBAL MANAGEMENT</h4>
-                        <h5 style={{marginBottom:"2px",color:"grey"}}>University</h5>
-                        <h5 style={{marginBottom:"2px",color:"grey", fontSize:"14px"}}>Top 20 in The World : FORBES INTERNATIONAL B-SCHOOLS (2013-15)</h5>
-                        <p style={{fontSize:"14px"}}>2017 - Present</p>
-                        <span>Bachelor of Business Administration (DEANS LIST) - Three cities program; one year in Singapore, one year in Dubai, and two years in Sydney. Coursework included Cost Accounting, Digital Marketing, Employability and Practitioner Skill, Financial Market and Institution, Financial Statement Analysis, Foundations of Entrepreneurship, Human Resource Management, Business Law, Decision Making, Project Management, Australia Business Environment, Branding and Communication, Strategic Management, Business Disruptive Technology, Business English Writing, Business Oral Communication, Computer Skills, Corporate Finance, Critical Thinking, Decision Science, Emotional Intelligence, Financial Accounting, Leadership, Macroeconomics, Marketing, Mathematics, Operations Management, Organisational Behaviour, Principles of Management, Regional Immersion Project, Research Methodology, Statistics, Sustainability & Corporate Social Responsibility, World Cultures. Average GPA of 4.0
-                        </span>
+                      <h4 style={{marginBottom:"2px"}}>GOFLEET INTERNATIONAL</h4>
+                      <h5 style={{marginBottom:"2px",color:"grey"}}>Management Trainee</h5>
+                      <p style={{marginBottom:"2px",fontSize:"13px"}}>May 2019 - Aug 2019</p>
+                      <p style={{fontSize:"12px"}}>Dubai, UAE</p>
+                      <span>Worked closely with the CEO, conducted a firm’s Target Operating Model(TOM)for its underground airport project, and prepared top management presentations for its clients, Dnata, Emirates Group, and Dubai Airport.</span>
+                      <br/><br/>
+                      <span>
+                      Collaborated with data analysts, engineers, and project teams in managing $20M+ worth of the vast amount of telematics data. Gathered and analyzed data & functionalities requirements from clients, applied data modeling techniques and designed workflow into a process map, investigated the data by assigning formulas, sorting, filtering, formatting, and transmuted them into charts, diagrams, and dashboards such as assets utilization matrix, fuel consumption matrix, vehicles idling time, ROI, etc. Followed by UAT(user acceptance testing) for big company’s clients, Dnata and Emirates Group.
+                      </span>
+                      <br/><br/>
+                      <span>
+                      Team up with senior engineers in scheduling project operational tasks by mapping and assigning resources for a range of tasks in meeting target dates. Formulated charts, Power BI and utilize management tools like Gantt chart to keep track and inform the project progress among stakeholders.
+                      </span>
+                      <br/><br/>
+                      <span>
+                      Worked together with project manager in mapping and designing workflow of product integration and product introduction to stakeholders for Dubai Airport Underground Project.
+                      </span>
+                      <br/><br/>
+                      <span>
+                      Conducted $300,000 worth of primary and secondary market research on the feasibility and viability of electric bikes in Dubai as a mode of delivery in the F&B sector. Coming up with entry strategy, competitive market analysis, and the possibility of the firm's diversification of its strategic breadth to the internal stakeholders.
+                      </span>
+                      <br/><br/>
+                      <span>
+                      Redesigning the company’s profile deck, website UI/UX, and brochures. Arranging Excel and PowerPoint training for the junior team. Involved in hiring, preparing tests, and selecting new potential candidates along with the HR manager and the CEO.</span>
                       </Col>
                       
                     </Row>
