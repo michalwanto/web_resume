fab5db483638af18dc06633871d70870cd9a13a5
michalwanto <59409764+michalwanto@users.noreply.github.com>
2020-11-03 11:47:08 +1100

    removed footer

--- src/components/Footers/DefaultFooter.js
+++ src/components/Footers/DefaultFooter.js
@@ -13,49 +13,10 @@ function DefaultFooter() {
         <Container>
           <nav>
             <ul>
-              <li>
-                <a
-                  href="https://www.creative-tim.com?ref=nukr-default-footer"
-                  target="_blank"
-                >
-                  Creative Tim
-                </a>
-              </li>
-              <li>
-                <a
-                  href="http://presentation.creative-tim.com?ref=nukr-default-footer"
-                  target="_blank"
-                >
-                  About Us
-                </a>
-              </li>
-              <li>
-                <a
-                  href="http://blog.creative-tim.com?ref=nukr-default-footer"
-                  target="_blank"
-                >
-                  Blog
-                </a>
-              </li>
+              
             </ul>
           </nav>
-          <div className="copyright" id="copyright">
-            © {new Date().getFullYear()}, Designed by{" "}
-            <a
-              href="https://www.invisionapp.com?ref=nukr-default-footer"
-              target="_blank"
-            >
-              Invision
-            </a>
-            . Coded by{" "}
-            <a
-              href="https://www.creative-tim.com?ref=nukr-default-footer"
-              target="_blank"
-            >
-              Creative Tim
-            </a>
-            .
-          </div>
+          
         </Container>
       </footer>
     </>
